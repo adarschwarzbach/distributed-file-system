cd17f07af1decce4a9cf385ac0aec2b418c87ee9
Elysia Ye <elysiaye@Elysias-MacBook-Pro.local>
2024-11-19 17:39:32 -0500

    filled in TODOs

--- src/coordinator/Coordinator.py
+++ src/coordinator/Coordinator.py
@@ -11,11 +11,11 @@ import json
 class Coordinator:
     def __init__(self, host='localhost', port=6000, max_workers=10):
         # maps file_id to a File object
-        file_map: Dict[int, File] = {}
+        self.file_map: Dict[int, File] = {}
         # maps chunk_id to the 3 ChunkServers where we can find that chunk
-        chunk_map: Dict[int, List[ChunkServer]] = {}
+        self.chunk_map: Dict[int, List[ChunkServer]] = {}
         # dict of ChunkServers that are online mapping to the number of chunks on that chunk server
-        active_chunkservers: Dict[ChunkServer, int] = {}
+        self.active_chunkservers: Dict[ChunkServer, int] = {}
 
         # Networking & threading
         self.host = host
@@ -42,8 +42,18 @@ class Coordinator:
             print(f"Received request: {request}")
             if request.get("request_type") == "GET_CLIENT_ID":
                 self.handle_get_client_id(client_socket)
-
             # Handle other request types below
+            elif request.get("request_type") == "ADD_CHUNK_SERVER":
+                self.handle_add_chunk_server(request.get("chunk_server"))
+            elif request.get("request_type") == "NEW_FILE":
+                new_file = request.get("file")
+                self.handle_new_file(File(**new_file))
+            elif request.get("request_type") == "DELETE_FILE":
+                file_id = request.get("file_id")
+                self.handle_delete_file(file_id)
+            else:
+                print("Unknown request type")
+            
         except json.JSONDecodeError:
             print("Invalid JSON received")
 
@@ -67,18 +77,22 @@ class Coordinator:
         '''
         makes network call to check if chunk_server is offline
         '''
-        # TODO: make network call -> return boolean, true for valid, false for offline
-        valid = True # just placeholder for now
-        if not valid:
-            del self.active_chunkservers[chunk_server]
-            self.handle_chunk_server_failure()
+        try: 
+            with socket.create_connection((chunk_server.host, chunk_server.port), timeout=5):
+                return True
+        except Exception:
+            return False
 
     def check_active_servers(self):
         '''
         go through self.active_chunkservers to see if any have gone offline unexpectedly
         '''
-        for chunk_server in self.active_chunkservers:
-            self.check_active_server(chunk_server)
+        offline_servers = []
+        for chunk_server in list(self.active_chunkservers):
+            if not self.check_active_server(chunk_server):
+                offline_servers.append(chunk_server)
+        for server in offline_servers:
+            self.handle_chunk_server_failure(server)
 
     def handle_chunk_server_failure(self, failed_server: ChunkServer):
         '''
@@ -87,12 +101,14 @@ class Coordinator:
         '''
         for chunk in failed_server.chunks:
             self.remap_chunk(chunk)
+        del self.active_chunkservers[failed_server]
 
     def handle_add_chunk_server(self, new_server: ChunkServer):
         '''
         handle request for a new ChunkServer to join
         '''
         # TODO: network call to connect chunk server
+        # !!!!!!
         self.active_chunkservers[new_server] = 0
         pass
 
@@ -103,6 +119,7 @@ class Coordinator:
         for chunk in server_to_remove.chunks:
             self.remap_chunk(chunk)
         del self.active_chunkservers[server_to_remove]
+        print(f"Removed chunk server: {server_to_remove}")
 
     def handle_new_file(self, new_file: File):
         '''
@@ -110,21 +127,23 @@ class Coordinator:
         '''
         for chunk in new_file:
             self.map_chunk_to_chunk_servers(chunk)
+        self.file_map[new_file.id] = new_file
+        print(f"New file handled: {new_file.id}")
 
-    def get_least_loaded_chunk_servers(self):
-        count_to_chunk_server = sorted([(count, chunk_server) for chunk_server, count in self.active_chunkservers])[:max(len(self.active_chunkservers), 3)]
-        return [server for _, server in count_to_chunk_server]
+    def get_least_loaded_chunk_servers(self) -> List[ChunkServer]:
+        sorted_servers = sorted(self.active_chunkservers.items(), key=lambda x: x[1])
+        return [server for _, server in sorted_servers[:3]]
 
     def map_chunk_to_chunk_servers(self, chunk: Chunk):
         '''
         map each new chunk to 3 chunkservers
         '''
-        # have some way of finding the 3 chunkservers that are least loaded at the moment
         chunk_servers = self.get_least_loaded_chunk_servers()
+        self.chunk_map[chunk.id] = chunk_servers
         for chunk_server in chunk_servers:
-            # TODO: make calls to chunk_server to add chunk to that chunk_server
+            chunk_server.add_chunk(chunk)
             self.active_chunkservers[chunk_server] += 1
-            pass
+        print(f"Chunk {chunk.id} mapped to servers: {chunk_servers}")
 
     def remap_chunk(self, chunk: Chunk):
         '''
@@ -139,8 +158,9 @@ class Coordinator:
         file = self.file_map[file_id_to_delete]
         for chunk in file.chunks:
             chunk_id = chunk.id
-            for chunk_server in self.chunk_map[chunk_id]:
-                # TODO: check is chunk_server is alive if so:
-                # TODO: send request to chunk_server to delete chunk with chunk_id
-                self.active_chunkservers[chunk_server] -= 1
-                pass
+            for chunk_id in self.chunk_map:
+                for chunk_server in self.chunk_map[chunk_id]:
+                    if self.check_active_server(chunk_server):
+                        chunk_server.delete_chunk(chunk_id)
+                        self.active_chunkservers[chunk_server] -= 1
+        del self.file_map[file_id_to_delete]
